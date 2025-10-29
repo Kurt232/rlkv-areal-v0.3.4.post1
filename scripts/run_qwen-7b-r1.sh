91e3b7b0f2966251cca54dbc1a944bf078f42eb6
duwenjie <duwenjie24@gmail.com>
2025-10-29 14:27:23 +0800

    [update] training recipe

--- scripts/run_qwen-7b-r1.sh
+++ scripts/run_qwen-7b-r1.sh
@@ -1,29 +1,31 @@
 set -e
-# export CUDA_VISIBLE_DEVICES=2,3
+export CUDA_VISIBLE_DEVICES=0,1
 
-expr_name=AReaL-GRPO-n4-streaming
-model=deepseek-ai/DeepSeek-R1-Distill-Llama-8B
-trial_name=$1
-lr=$2
-epochs=4
-reg_loss_scale=$3
-adapter_init_value=$4
+expr_name=AReaL-GRPO-n4-streaming-llama
+model=deepseek-ai/DeepSeek-R1-Distill-Qwen-7B
+
+lr=1e-2
+epochs=2
+reg_loss_scale=1e-3
+reg_loss_tau=0.55
+adapter_init_value=1.0
 sink_win_size=128
 recent_win_size=256
-trial_name="laser__${trial_name}"
+trial_name="qwen7b_cfg0_t0.55__lr1e-2_ep2_bs32_reg1e-3"
 
-python3 -m areal.launcher.local examples/lite/gsm8k_grpo.py --config examples/lite/configs/gsm8k_grpo.yaml \
+python3 -m areal.launcher.local examples/lite/gsm8k_grpo.py --config examples/rlkv/deepseek_grpo.yaml \
     experiment_name="${expr_name}" \
     trial_name="${trial_name}" \
     total_train_epochs="${epochs}" \
-    ++train_dataset.path=Kurt232/Sampled-Laser-Dataset \
-    ++valid_dataset.path=Kurt232/Sampled-Laser-Dataset \
+    ++train_dataset.path=Kurt232/Sampled-Laser-Dataset-Qwen-2.5-7B-R1 \
+    ++valid_dataset.path=Kurt232/Sampled-Laser-Dataset-Qwen-2.5-7B-R1 \
     ++actor.path="${model}" \
     ++actor.optimizer.lr="${lr}" \
     ++actor.enable_mixed_attn_training="true" \
     ++actor.sink_window_size="${sink_win_size}" \
     ++actor.recent_window_size="${recent_win_size}" \
     ++actor.reg_loss_scale="${reg_loss_scale}" \
+    ++actor.reg_loss_tau="${reg_loss_tau}" \
     ++actor.adapter_init_value="${adapter_init_value}" \
     ++sglang.attention_backend="triton" \
     ++sglang.enable_mixed_attention="true" \
--- scripts/run_qwen3-4b-thinking.sh
+++ scripts/run_qwen3-4b-thinking.sh
@@ -1,33 +1,36 @@
 set -e
 export CUDA_VISIBLE_DEVICES=0,1
 
-expr_name=AReaL-GRPO-n4-streaming-llama
-model=deepseek-ai/DeepSeek-R1-Distill-Llama-8B
-trial_name=$1
-lr=$2
-epochs=4
-reg_loss_scale=$3
-adapter_init_value=$4
+expr_name=AReaL-GRPO-n4-streaming-qwen3
+model=Qwen/Qwen3-4B-Thinking-2507
+trial_name=lr1e-2_ep2_bs32_reg2.5e-3
+lr=1e-2
+epochs=2
+reg_loss_scale=2.5e-3
+reg_loss_tau=0.5
+adapter_init_value=1.0
 sink_win_size=128
 recent_win_size=256
-trial_name="laser_llama_v4__${trial_name}"
+trial_name="qwen3_cfg1_t0.5__lr1e-2_ep2_bs32_reg2.5e-3"
 
-python3 -m areal.launcher.local examples/lite/gsm8k_grpo.py --config examples/lite/configs/gsm8k_grpo.yaml \
+python3 -m areal.launcher.local examples/math/gsm8k_grpo.py --config examples/rlkv/qwen3_grpo.yaml \
     experiment_name="${expr_name}" \
     trial_name="${trial_name}" \
     total_train_epochs="${epochs}" \
-    ++train_dataset.path=Kurt232/Sampled-Laser-Dataset-V4 \
-    ++valid_dataset.path=Kurt232/Sampled-Laser-Dataset-V4 \
+    ++allocation_mode=sglang:d1t2+fsdp:d1c2 \
+    ++train_dataset.path=Kurt232/Sampled-Laser-Dataset-Qwen-3-4B-Thinking \
+    ++valid_dataset.path=Kurt232/Sampled-Laser-Dataset-Qwen-3-4B-Thinking \
     ++actor.path="${model}" \
     ++actor.optimizer.lr="${lr}" \
     ++actor.enable_mixed_attn_training="true" \
     ++actor.sink_window_size="${sink_win_size}" \
     ++actor.recent_window_size="${recent_win_size}" \
     ++actor.reg_loss_scale="${reg_loss_scale}" \
+    ++actor.reg_loss_tau="${reg_loss_tau}" \
     ++actor.adapter_init_value="${adapter_init_value}" \
     ++sglang.attention_backend="triton" \
     ++sglang.enable_mixed_attention="true" \
     ++sglang.sink_window_size="${sink_win_size}" \
     ++sglang.recent_window_size="${recent_win_size}" \
     ++sglang.adapter_init_value="${adapter_init_value}" \
-    ++stats_logger.wandb.mode="online"
+    ++stats_logger.wandb.mode="disabled"
